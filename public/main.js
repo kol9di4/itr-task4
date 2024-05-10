4a6a095936e2968fa5814b27703a800116e9e1aa
kol9di4 <kol9di4@gmail.com>
2024-05-10 12:41:03 +0300

    fc

---
+++ public/main.js
@@ -0,0 +1,83 @@
+$(function(){
+    // baseUrl = window.location.host;
+    baseUrl="";
+    console.log(baseUrl);
+
+    function selectHighlightedUsers(){
+        idUsers = {};
+        i=-1;
+        $('input[type=checkbox]').each(function(){
+            if(this.checked === true && this.getAttribute('name') !== 'all'){
+                idUsers[++i]=(this.getAttribute('name'));
+            }
+        });
+        return idUsers;
+    }
+
+    function ajaxRequest (path)
+    {
+        data = selectHighlightedUsers();
+        $.ajax({
+            url: path, 
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+            },
+            data: data,
+            success: function (response) {
+                if (response!= null){
+                    response = JSON.parse(response);
+                    $('table').html(response);
+                }
+            },
+            error: function (error) {
+                alert("Ошибка при отправке данных: ", error);
+            },
+        });        
+    }
+
+    function checkStateMainCheckbox(){
+        checkBoxes = $('tbody input[type=checkbox]');
+        numberIncluded = 0;
+        checkBoxes.each(function(){
+            if(this.checked)
+                ++numberIncluded;
+        })
+        $('input[type=checkbox][name=all]').prop('checked',false);
+        if(numberIncluded === checkBoxes.length)
+            $('input[type=checkbox][name=all]').prop('checked',true);
+    }
+
+    $(document.body).on('change', 'input[type=checkbox][name=all]',function(){
+        $('input[type=checkbox]').prop('checked',this.checked);  
+    })
+
+
+    $(document.body).on('click', 'tbody tr', function(event) {
+        checkBox = $(this).find('input[type=checkbox]');
+        checkBox.each(function() {
+            $(this).prop("checked", !$(this).prop("checked"));
+        });
+        checkStateMainCheckbox();
+    })
+
+    $(document.body).on('click', 'tbody input[type=checkbox]', function(event) {
+        $(this).prop("checked", !$(this).prop("checked"));
+        checkStateMainCheckbox();
+    });
+
+    $('#block').on('click',function(){
+        ajaxRequest(baseUrl+"/user/block");
+        location.reload();
+    });
+    
+    $('#unblock').on('click',function(){
+        ajaxRequest(baseUrl+"/user/unblock");
+    });
+
+    $('#delete').on('click',function(){
+        ajaxRequest(baseUrl+"/user/delete");
+        location.reload();
+    });
+})
